07eb949ee10abf359d3b73156a80a8baa1594227
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-08-30 15:21:22 -0300

    estrutura html

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <header></header>      
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
